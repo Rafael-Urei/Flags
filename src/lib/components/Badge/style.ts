ebf80dd15f993e062597c041a236945ac9d822ed
rafael-atendo <rafael.souza@atendo.com>
2024-11-23 12:04:46 -0300

    change the badge style

--- src/lib/components/Badge/style.ts
+++ src/lib/components/Badge/style.ts
@@ -2,6 +2,6 @@ import { tv } from 'tailwind-variants';
 
 export const style = tv({
   slots: {
-    base: 'font-semibold text-sm rounded px-4 py-2 shadow-md h-max w-max border'
+    base: 'font-semibold text-sm rounded px-4 py-1 shadow-md h-max w-max border'
   }
 });
